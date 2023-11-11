c2216ab6e779235423915b65db9b4f1ec69ec5f5
최서지 <seoji@MacBook-Air.local>
2023-11-11 20:46:55 +0900

    [feat]: 진짜 모르겠다. 다음에 다시 도전

--- graph/4991-로봇청소기/로봇 청소기.cpp
+++ graph/4991-로봇청소기/로봇 청소기.cpp
@@ -1,24 +1,21 @@
 #include <iostream>
-#include <queue>
-#include <cstring>
+#include <vector>
 using namespace std;
 
 int w, h;
-int dirty_place;
-int ans, flag;
-char map[21][21];  // . : 깨끗한 칸, * : 더러운 칸, x : 가구, o : 로봇 청소기 시작 위치
-int dx[4] = {0, 1, 0, -1};
-int dy[4] = {1, 0, -1, 0};
-queue<pair<int, int>> q;
-int visited[21][21];
+int ans;
+int board[21][21];
+vector<pair<int, int>> dirt_place;  // 인덱스가 더러운 지점의 번호가 됨
+vector<pair<int, int>> dist;   // first: 더러운 지점, second: 그 지점까지의 거리
+int visited[500];
 
-void init()
+void init(void)
 {
+    ans = 0;
     memset(visited, 0, sizeof(visited));
-    while (!q.empty())
-    {
-        q.pop();
-    }
+    memset(board, 0, sizeof(board));
+    dirt_place.clear();
+    dist.clear();
 }
 
 int input(void)
@@ -26,55 +23,24 @@ int input(void)
     cin >> w >> h;
     if (w == 0 && h == 0)
         return (1);
-    dirty_place = 0;
     for(int i = 0; i < h; i++)
     {
         for(int j = 0; j < w; j++)
         {
-            cin >> map[i][j];
-            if (map[i][j] == '*')
-                dirty_place++;
+            cin >> board[i][j];
+            if (board[i][j] == '*')
+                dirt_place.push_back({i, j});   // 더러운 곳 위치 저장
         }
     }
     return (0);
 }
 
-int solve(void)
+void get_distance(void)
 {
-    int cnt = 0;
-    int q_size;
-
-    while (!q.empty())
+    for(int i = 0; i < dirt_place.size(); i++)
     {
-        cnt++;
-        q_size = q.size();
-        while (q_size--)
-        {
-            int x = q.front().second;
-            int y = q.front().first;
-            q.pop();
-
-            for(int i = 0; i < 4; i++)
-            {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-
-                if (nx >= 0 && nx < w && ny >= 0 && ny < h && !visited[ny][nx])
-                {
-                    if (map[ny][nx] == 'x')
-                        continue;
-                    if (map[ny][nx] == '*')  // 가장 가까운 더러운 위치
-                    {
-                        map[ny][nx] = 'o';  // 다시 시작위치로 바꿔주고
-                        return (cnt);
-                    }
-                    q.push({ny, nx});
-                    visited[ny][nx] = 1;
-                }
-            }
-        }
+        
     }
-    return (-1);
 }
 
 int main(void)
@@ -85,36 +51,12 @@ int main(void)
     while (1)
     {
         if (input())
-            break ;
-        ans = 0;
-        while (dirty_place--)
-        {
-            flag = 0;
-            for(int i = 0; i < h; i++)
-            {
-                for(int j = 0; j < w; j++)
-                {
-                    if (map[i][j] == 'o')
-                    {
-                        init();
-                        map[i][j] = '.';
-                        q.push({i, j});
-                        visited[i][j] = 1;
-                        flag = solve();
-                        break ;
-                    }
-                }
-                if (flag != 0)
-                    break ;
-            }
-            if (flag == -1)
-            {
-                ans = -1;
-                break;
-            }
-            ans += flag;
-        }
+            break;
+        init();
+        get_distance();
+
         cout << ans << "\n";
     }
+
     return (0);
 }
