e82c5ada33d943e1d619faddb223131a921fdaa1
choiseoji <seoji@MacBook-Air.local>
2024-02-25 21:42:58 +0900

    [feat]: dp 활용한 문제

--- dp/2011-암호코드/암호코드.cpp
+++ dp/2011-암호코드/암호코드.cpp
@@ -1,26 +1,34 @@
 #include <iostream>
 using namespace std;
 
+string num;
+long long dp[5001];
+
 int main(void)
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    string num;
-    long long dp[5001];
-
     cin >> num;
-    dp[0] = dp[1] = 1;
-    if (num[0] == '1' || (num[0] == '2' && num[1] <= '6'))
-        dp[1] += 1;
-    for(int i = 2; i < num.size(); i++)
+
+    if (num[0] == '0')
     {
-        // 현재 글자만 
-        dp[i] = dp[i - 1];
-        // 앞에 글자까지 한 글자로 보면
-        if (num[i - 1] == '1' || (num[i - 1] == '2' && num[i] <= '6'))
-            dp[i] += (dp[i - 2] % 1000000);
+        cout << 0;
+        return (0);
     }
-    cout << dp[num.size() - 1];
+
+    dp[0] = dp[1] = 1;    // num은 인덱스 0부터 시작, dp는 1부터 시작
+
+    for(int i = 2; i <= num.size(); i++)
+    {
+        // 현재가 0이 아니라면 dp[i - 1]의 경우의 수 그대로
+        if (num[i - 1] != '0')
+            dp[i] = dp[i - 1] % 1000000;
+
+        // 현재와 이전 숫자가 하나의 알파벳이 된다면 dp[i - 2]의 경우의 수 받기
+        if (num[i - 2] == '1' || (num[i - 2] == '2' && num[i - 1] <= '6'))
+            dp[i] = (dp[i] + dp[i - 2]) % 1000000;
+    }
+    cout << dp[num.size()];
     return (0);
 }
